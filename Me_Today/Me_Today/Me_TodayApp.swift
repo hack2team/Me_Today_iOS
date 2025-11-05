7a25e7531b211fc7477c32221a521c77e345e283
circle0802 <hawon9781@gmail.com>
2025-11-05 23:35:50 +0900

    프로젝트 생성

---
+++ Me_Today/Me_Today/Me_TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Me_TodayApp.swift
+//  Me_Today
+//
+//  Created by 원이 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Me_TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Me_Today/Me_TodayTests/Me_TodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Me_TodayTests.swift
+//  Me_TodayTests
+//
+//  Created by 원이 on 11/5/25.
+//
+
+import Testing
+@testable import Me_Today
+
+struct Me_TodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
