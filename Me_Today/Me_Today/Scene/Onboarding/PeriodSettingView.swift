760714b2c8cffdb7a140b700501ee1fd7893d794
circle0802 <hawon9781@gmail.com>
2025-11-06 01:11:21 +0900

    뷰이동

---
+++ Me_Today/Me_Today/Scene/Onboarding/PeriodSettingView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct PeriodSettingView: View {
+    var body: some View {
+        Text("PeriodSettingView")
+    }
+}
--- Me_Today/Me_Today/Scene/Onboarding/StartView.swift
+++ Me_Today/Me_Today/Scene/Onboarding/StartView.swift
@@ -2,26 +2,28 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        ZStack {
-            Image("background")
-            VStack {
-                Image("logoText")
-                VStack (spacing: 0) {
-                    Image("onboardingBear")
-                    Button(action: {}, label: {
-                        Text("시작하기")
-                            .font(.pretendard(.semibold, size: 16))
-                            .padding(.vertical, 17)
-                            .frame(maxWidth: .infinity)
-                    })
-                    .foregroundStyle(Color.blue100)
-                    .background(Color.white)
-                    .cornerRadius(42)
-                    .padding(.horizontal, 30)
+        NavigationStack {
+            ZStack {
+                Image("background")
+                VStack {
+                    Image("logoText")
+                    VStack(spacing: 0) {
+                        Image("onboardingBear")
+                        NavigationLink(destination: PeriodSettingView()) {
+                            Text("시작하기")
+                                .font(.pretendard(.semibold, size: 16))
+                                .padding(.vertical, 17)
+                                .frame(maxWidth: .infinity)
+                                .background(Color.white)
+                                .foregroundStyle(Color.blue100)
+                                .cornerRadius(42)
+                        }
+                        .padding(.horizontal, 30)
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
