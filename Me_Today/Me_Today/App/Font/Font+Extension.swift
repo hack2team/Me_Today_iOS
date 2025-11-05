3ab8161002f2bdf8e516df8082ecb2704a5c2490
circle0802 <hawon9781@gmail.com>
2025-11-06 02:56:38 +0900

    폰트 추가

--- Me_Today/Me_Today/App/Font/Font+Extension.swift
+++ Me_Today/Me_Today/App/Font/Font+Extension.swift
@@ -5,9 +5,28 @@ enum PretendardFont: String {
     case medium = "Pretendard-Medium"
     case semibold = "Pretendard-Semibold"
 }
+enum WantedSansFont: String {
+    case bold = "WantedSans-Bold"
+    case regular = "WantedSans-Regular"
+}
+enum GangwonFont: String {
+    case extraBold = "Gangwon-ExtraBold"
+}
+enum PyeongChangFont: String {
+    case bold = "PyeongChang-Bold"
+}
 
 extension Font {
     static func pretendard(_ style: PretendardFont, size: CGFloat) -> Font {
+        return .custom(style.rawValue, size: size)
+    }
+    static func wantedSans(_ style: WantedSansFont, size: CGFloat) -> Font {
+        return .custom(style.rawValue, size: size)
+    }
+    static func gangwon(_ style: GangwonFont, size: CGFloat) -> Font {
+        return .custom(style.rawValue, size: size)
+    }
+    static func pyeongChang(_ style: PyeongChangFont, size: CGFloat) -> Font {
         return .custom(style.rawValue, size: size)
     }
 }
