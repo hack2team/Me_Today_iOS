2741e7459515ca2d732bd1df77ef258278fa02fe
circle0802 <hawon9781@gmail.com>
2025-11-06 05:46:34 +0900

    얏호 리포트 제작하기 전 뷰 퍼블리싱

--- Me_Today/Me_Today/Scene/Report/ReportView.swift
+++ Me_Today/Me_Today/Scene/Report/ReportView.swift
@@ -2,6 +2,20 @@ import SwiftUI
 
 struct ReportView: View {
     var body: some View {
-        Text("report")
+        NavigationBarView()
+        VStack {
+            Image("reportIcon")
+                .padding(.top, 82)
+            Button(action: {}, label: {
+                Text("제작하기")
+                    .font(.pretendard(.semibold, size: 16))
+                    .padding(.vertical, 14)
+                    .foregroundStyle(.white)
+            })
+            .frame(width: 236)
+            .background(Color.blue100)
+            .cornerRadius(100)
+            .offset(y: -100)
+        }
     }
 }
